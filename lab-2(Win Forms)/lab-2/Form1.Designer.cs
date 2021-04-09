81ee1302fa5c0eb686aa12d9519d77e1e43cda19
vladzo18 <71393833+vladzo18@users.noreply.github.com>
2021-04-09 13:43:05 +0300

    Add lab-2(Win Forms)

---
+++ lab-2(Win Forms)/lab-2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 11);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(413, 222);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(405, 196);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вкладка 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(405, 196);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Вкладка 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(155, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 243);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab-2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ lab-2(Win Forms)/lab-2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            ButtonsField bf = new ButtonsField(textBox2);
+            bf.drawField(tabPage2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!(textBox1.Text == ""))
+            {
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox1.Items.RemoveAt(comboBox1.Items.Count - 1);
+            }
+            catch
+            {
+                MessageBox.Show("И что мне удалять?", "Лист пуст!!!");
+            }
+        }
+    }
+
+    class ButtonsField
+    {
+        #region Fields
+        private List<Button> Buttons = new List<Button>();
+        private sbyte amountButtons = 16;
+        private sbyte count = 0;
+        private object textBoxForResult;
+        #endregion
+        public ButtonsField(object textBoxForResult)
+        {
+            sbyte yLocation = 28;
+
+            for (int i = 0; i < amountButtons; i++)
+            {
+                Buttons.Add(new Button());
+
+                Buttons[i].Text = (i + 1).ToString();
+                Buttons[i].TabStop = false;
+                Buttons[i].Size = new Size(30, 20);
+                Buttons[i].Location = new Point(75 * ((i % 4) + 1), yLocation);
+
+                Buttons[i].Click += new EventHandler(btn_handler);
+
+                if ((i + 1) % 4 == 0)
+                {
+                    yLocation += 30;
+                }
+            }
+
+            this.textBoxForResult = (textBoxForResult);
+        }
+
+        public void drawField(object getter)
+        {
+            shuffleButtons();
+            for (int i = 0; i < amountButtons; i++)
+                (getter as TabPage).Controls.Add(Buttons[i]);
+        }
+        public void shuffleButtons()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < amountButtons; i++)
+            {
+                int j = rand.Next(i);
+
+                Point tmp = Buttons[j].Location;
+                Buttons[j].Location = Buttons[i].Location;
+                Buttons[i].Location = tmp;
+
+            }
+        }
+
+        private void resetHidingButtons ()
+        {
+            shuffleButtons();
+            for (int i = 0; i < amountButtons; i++)
+                if (!Buttons[i].Visible)
+                    Buttons[i].Show();
+
+            count = 0;
+        }
+
+        private void btn_handler(object sender, EventArgs e)
+        {
+            if (count == (amountButtons - 1))
+            {
+                (textBoxForResult as TextBox).Text = "Молодец!!!";
+                resetHidingButtons();
+            }
+            else
+            {
+                (textBoxForResult as TextBox).Text = "";
+            }
+
+            if ((sender as Button).Text == (count + 1).ToString())
+            {
+                shuffleButtons();
+                (sender as Button).Hide();
+                count++;
+            }
+            else
+            {
+                resetHidingButtons();
+            }
+        }
+    }
+}
