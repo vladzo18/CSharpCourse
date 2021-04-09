3555688eca0378c4aff3f7ac79d562e518379ee7
vladzo18 <71393833+vladzo18@users.noreply.github.com>
2021-04-09 21:31:10 +0300

    Add lab-1(WPF)

---
+++ lab-1(WPF)/test-wpf/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace test_wpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            foreach (UIElement el in mainRoot.Children) {
+                if(el is Button)
+                    (el as Button).Click += button_Click;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+                textBox.Text = "";
+            else if(str == "=") {
+                string value = new DataTable().Compute(textBox.Text, null).ToString();
+                textBox.Text = value;
+            }
+            else
+                textBox.Text += str;
+        }
+    }
+}
