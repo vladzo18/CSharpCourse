d2150e927d70e597f7789fcdf8edeae68c8b4565
vladzo18 <71393833+vladzo18@users.noreply.github.com>
2021-04-11 23:10:48 +0300

    Add lab-3(Win Forms)

---
+++ lab-3(Win Forms)/lab-3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerForButton = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(251, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 57);
+            this.button1.TabIndex = 2;
+            this.button1.TabStop = false;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cansel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timerForButton
+            // 
+            this.timerForButton.Enabled = true;
+            this.timerForButton.Interval = 40;
+            this.timerForButton.Tick += new System.EventHandler(this.TimerForButton_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 374);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "lab-3";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerForButton;
+    }
+}
+
---
+++ lab-3(Win Forms)/lab-3/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab_3
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random();
+        private int counter = 0;
+        private bool buttonMinimiz = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Controls.SetChildIndex(button1, 0);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ты меня поймал!!!", "Хитрец");
+        }
+
+        private void Button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point(rand.Next(this.Width / 2), rand.Next(this.Height / 2));
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Y >= button1.Location.Y && e.Y <= button1.Location.Y + button1.Height)
+            {
+                if ((e.X >= button1.Location.X - button1.Width && e.X <= button1.Location.X))
+                {
+                    button1.Location = new Point(button1.Location.X + 1, button1.Location.Y);
+                    buttonMinimiz = true;
+                }
+                if ((e.X <= button1.Location.X + button1.Width * 2 && e.X >= button1.Location.X))
+                {
+                    button1.Location = new Point(button1.Location.X - 1, button1.Location.Y);
+                    buttonMinimiz = true;
+                }
+            }
+            else
+            {
+                buttonMinimiz = false;
+            }
+
+            if (e.X >= button1.Location.X && e.X <= button1.Location.X + button1.Width)
+            {
+                if ((e.Y >= button1.Location.Y - button1.Height && e.Y <= button1.Location.Y))
+                {
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y + 1);
+                    buttonMinimiz = true;
+                }
+                if ((e.Y <= button1.Location.Y + button1.Height * 2 && e.Y >= button1.Location.Y))
+                {
+                    button1.Location = new Point(button1.Location.X, button1.Location.Y - 1);
+                    buttonMinimiz = true;
+                }
+            }
+            else
+            {
+                buttonMinimiz = false;
+            }
+
+            if (e.X >= button1.Location.X - button1.Width && e.X <= button1.Location.X)
+            {
+                if (e.Y >= button1.Location.Y - button1.Height && e.Y <= button1.Location.Y)
+                {
+                    button1.Location = new Point(button1.Location.X + 1, button1.Location.Y + 1);
+                    buttonMinimiz = true;
+                }
+                if (e.Y <= button1.Location.Y + button1.Height * 2 && e.Y >= button1.Location.Y)
+                {
+                    button1.Location = new Point(button1.Location.X + 1, button1.Location.Y - 1);
+                    buttonMinimiz = true;
+                }
+            }
+            else
+            {
+                buttonMinimiz = false;
+            }
+            if (e.X <= button1.Location.X + button1.Width * 2 && e.X >= button1.Location.X)
+            {
+                if (e.Y >= button1.Location.Y - button1.Height && e.Y <= button1.Location.Y)
+                {
+                    button1.Location = new Point(button1.Location.X - 1, button1.Location.Y + 1);
+                    buttonMinimiz = true;
+                }
+                if (e.Y <= button1.Location.Y + button1.Height * 2 && e.Y >= button1.Location.Y)
+                {
+                    button1.Location = new Point(button1.Location.X - 1, button1.Location.Y - 1);
+                    buttonMinimiz = true;
+                }
+            }
+            else
+            {
+                buttonMinimiz = false;
+            }
+
+            if ((button1.Location.X + button1.Width >= this.Width) 
+                || (button1.Location.X <= 0) 
+                || (button1.Location.Y <= 0)
+                || (button1.Location.Y + button1.Height*2.5 >= this.Height))
+            {
+                button1.Location = new Point(rand.Next(this.Width / 2), rand.Next(this.Height / 2));
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (counter % 2 != 0 && counter <= 16)
+                this.Text = "Нажмите кнопку \"Ок\"!!!";
+            else
+                this.Text = "";
+
+            if(button1.Height == 0)
+            {
+                if (counter % 2 != 0)
+                    this.Text = "Кнопка \"Ок\" не может быть нажата";
+                else
+                    this.Text = "";
+            }
+
+            counter++;
+        }
+        private void TimerForButton_Tick(object sender, EventArgs e)
+        {
+            if(buttonMinimiz)
+                button1.Height -= 1;
+            if (button1.Height <= 5)
+                button1.Height = 0;
+        }
+    }
+}
