468ebe01d90e9710750c6a0faa85d93826439ed4
vladzo18 <71393833+vladzo18@users.noreply.github.com>
2021-04-11 20:21:52 +0300

    Add lab-2(WPF)

---
+++ lab-2(WPF)/lab-2_(WPF)/ButtonField.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab_2__WPF_
+{
+    public class ButtonsField
+    {
+        private List<Button> Buttons = new List<Button>();
+        private sbyte amountButtons = 16;
+        private sbyte count = 0;
+        private object textBoxForResult;
+
+        public ButtonsField(object textBoxForResult)
+        {
+            for (int i = 0; i < amountButtons; i++)
+            {
+                Buttons.Add(new Button());
+
+                Buttons[i].Content = (i + 1).ToString();
+
+                Buttons[i].Width = 30;
+                Buttons[i].Height = 20;
+
+                Buttons[i].Click += btn_handler;
+            }
+
+            this.textBoxForResult = (textBoxForResult);
+        }
+
+        public void drawField(Grid getter)
+        {
+            sbyte x = 0, y = 0;
+
+            for (int i = 0; i < amountButtons; i++)
+            {
+                Grid.SetColumn(Buttons[i], 0 + x);
+                Grid.SetRow(Buttons[i], 0 + y);
+                getter.Children.Add(Buttons[i]);
+
+                x++;
+                if ((i + 1) % 4 == 0)
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+            shuffleButtons();
+        }
+
+        public void shuffleButtons()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < amountButtons; i++)
+            {
+                sbyte j = (sbyte)rand.Next(i);
+
+                Point some = new Point(Grid.GetColumn(Buttons[j]), Grid.GetRow(Buttons[j]));
+
+                Grid.SetColumn(Buttons[j], Grid.GetColumn(Buttons[i]));
+                Grid.SetRow(Buttons[j], Grid.GetRow(Buttons[i]));
+
+                Grid.SetColumn(Buttons[i], (int)some.X);
+                Grid.SetRow(Buttons[i], (int)some.Y);
+            }
+        }
+        private void resetHidingButtons()
+        {
+            shuffleButtons();
+            for (int i = 0; i < amountButtons; i++)
+                if (Buttons[i].Visibility == Visibility.Hidden)
+                    Buttons[i].Visibility = Visibility.Visible;
+            count = 0;
+        }
+
+        private void btn_handler(object sender, EventArgs e)
+        {
+            if (count == (amountButtons - 1))
+            {
+                (textBoxForResult as TextBox).Text = "Молодец!!!";
+                resetHidingButtons();
+            }
+            else
+                (textBoxForResult as TextBox).Text = "";
+
+            if ((sender as Button).Content.ToString() == (count + 1).ToString())
+            {
+                (sender as Button).Visibility = Visibility.Hidden;
+                count++;
+                shuffleButtons();
+            }
+            else
+            {
+                resetHidingButtons();
+                count = 0;
+            }
+        }
+    }
+}
---
+++ lab-2(WPF)/lab-2_(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace lab_2__WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ButtonsField bf = new ButtonsField(textBox2);
+            bf.drawField(gridForButtons);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                comboBox.Items.Add(textBox.Text);
+                textBox.Text = "";
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.Items.Count != 0)
+                comboBox.Items.RemoveAt(comboBox.Items.Count - 1);
+            else
+                MessageBox.Show("И что мне удалять?", "Комбо Бокс пустой");
+        }
+    }
+}
