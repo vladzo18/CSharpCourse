0a793b53d0d6a10b72cca51c30eb01e3fb1163f5
vladzo18 <71393833+vladzo18@users.noreply.github.com>
2021-05-13 22:09:23 +0300

    Add lab-9(WPF)

---
+++ lab-9(WPF)/lab-9(WPF)/CaesarCipher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9_WPF_
+{
+    public static class CaesarCipher
+    {
+        const string alfabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
+
+        private static string CodeOrEncode(string text, int key) {
+            string fullAlfabet = alfabet + alfabet.ToLower();
+            int letterQty = fullAlfabet.Length;
+            string retVal = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char codedChar = text[i];
+                int index = fullAlfabet.IndexOf(codedChar);
+                if (index < 0)
+                {
+                    retVal += codedChar.ToString();
+                }
+                else
+                {
+                    int codeIndex = (letterQty + index + key) % letterQty;
+                    retVal += fullAlfabet[codeIndex];
+                }
+            }
+
+            return retVal;
+        }
+        public static string Encrypt(string plainMessage, int key)
+        {
+            return CodeOrEncode(plainMessage, -key);
+        }
+        public  static string Decrypt(string encryptedMessage, int key)
+        {
+            return CodeOrEncode(encryptedMessage, key);
+        }
+    }
+}
---
+++ lab-9(WPF)/lab-9(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_9_WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.DefaultExt = ".txt";
+            fileDialog.Filter = "Text files (*.txt)|*.txt";
+            var result = fileDialog.ShowDialog();
+
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    TxtFile.Content = file;
+
+                    using (FileStream fs = new FileStream(fileDialog.FileName, FileMode.Open))
+                    {
+                        using (TextReader sr = new StreamReader(fs, Encoding.Default))
+                        {
+                            try
+                            {
+                                if (decryptRadio.IsChecked == true)
+                                {
+                                    textArea.Text = CaesarCipher.Decrypt(sr.ReadToEnd(), Int32.Parse(slideValue.Text));
+                                }
+                                else
+                                {
+                                    textArea.Text = CaesarCipher.Encrypt(sr.ReadToEnd(), Int32.Parse(slideValue.Text));
+                                }
+                            }
+                            catch 
+                            {
+                                MessageBox.Show("Проверь все ли поля заполнени", "Ейй");
+                            }
+                        }
+                    }
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    TxtFile.Content = "";
+                    break;
+            }
+        }
+    }
+}
